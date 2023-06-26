b13d54327833d3b5b5b0cb23bdad492bb6979a5e
Nicholas Lever <nik@catalystpics.co.uk>
2023-06-26 11:20:12 +0100

    Added Instancing to Build Settings

--- Assets/Scripts/HLSL/Raymarch.hlsl
+++ Assets/Scripts/HLSL/Raymarch.hlsl
@@ -1,5 +1,6 @@
 ﻿//Based on code from DMEville https://www.youtube.com/watch?v=0G8CVQZhMXw
 
+//Uses 3D texture and lighting 
 void raymarch_float( float3 rayOrigin, float3 rayDirection, float numSteps, float stepSize,
                      float densityScale, UnityTexture3D volumeTex, UnitySamplerState volumeSampler,
                      float3 offset, float numLightSteps, float lightStepSize, float3 lightDir,
@@ -13,22 +14,30 @@ void raymarch_float( float3 rayOrigin, float3 rayDirection, float numSteps, floa
     
 	for(int i =0; i< numSteps; i++){
 		rayOrigin += (rayDirection*stepSize);
-					
+
+		//The blue dot position
 		float3 samplePos = rayOrigin+offset;
 		float sampledDensity = SAMPLE_TEXTURE3D(volumeTex, volumeSampler, samplePos).r;
 		density += sampledDensity*densityScale;
+
 		//light loop
 		float3 lightRayOrigin = samplePos;
 		
 		for(int j = 0; j < numLightSteps; j++){
+			//The red dot position
 			lightRayOrigin += -lightDir*lightStepSize;
 			float lightDensity = SAMPLE_TEXTURE3D(volumeTex, volumeSampler, lightRayOrigin).r;
+			//The accumulated density from samplePos to the light - the higher this value the less light reaches samplePos
 			lightAccumulation += lightDensity;
 		}
 
+		//The amount of light received along the ray from param rayOrigin in the direction rayDirection
         float lightTransmission = exp(-lightAccumulation);
+		//shadow tends to the darkness threshold as lightAccumulation rises
 		float shadow = darknessThreshold + lightTransmission * (1.0 -darknessThreshold);
+		//The final light value is accumulated based on the current density, transmittance value and the calculated shadow value 
 		finalLight += density*transmittance*shadow;
+		//Initially a param its value is updated at each step by lightAbsorb, this sets the light lost by scattering
 		transmittance *= exp(-density*lightAbsorb);
 					
 	}
